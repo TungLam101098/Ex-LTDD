ca668f3fbcf727dce8fbd610911bb73f80d83f20
TungLam101098 <n.t.lam10101998@gmail.com>
2020-07-05 00:37:41 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        button32.setOnClickListener(){
+            val intent: Intent = Intent( getApplicationContext(), MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
